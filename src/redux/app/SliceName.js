37cd5fdcd1f77f08c11e2f70d2e136f28de5f461
Mohammad Erfanul Islam Sifat <erfan@orangetoolz.com>
2023-05-27 10:20:54 +0600

    feat: add some datatable

---
+++ src/redux/app/SliceName.js
@@ -0,0 +1,12 @@
+export const SliceName = {
+	admin_login: '',
+	user_login: '',
+	admin: '',
+	setting: '',
+	location: '',
+	area: '',
+	employee: '',
+	client: '',
+	clientDeposite: '',
+	clientLoan: '',
+};
